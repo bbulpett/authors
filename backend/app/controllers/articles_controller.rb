8ba7d467a0aa45152ed48aee790134fcd9f55ab4
Barnabas Bulpett <bbulpett@charter.net>
2016-04-22 23:56:51 -0400

    Add resource serializers for Articles

--- backend/app/controllers/articles_controller.rb
+++ backend/app/controllers/articles_controller.rb
@@ -1,2 +1,11 @@
 class ArticlesController < JSONAPI::ResourceController
+	def show
+		article = Article.find(params[:id])
+
+		resource = ArticleResource.new(article, nil)
+
+		serializer = JSONAPI::ResourceSerializer.new(ArticleResource)
+
+		render json: serializer.serialize_to_hash(resource)
+	end
 end
